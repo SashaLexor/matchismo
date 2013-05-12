eb47be7bf8f6ff79ee6461c1004d39f109bc9992
SashaLexor <sashalexor@gmail.com>
2013-05-12 15:48:13 +0300

    adding tap bar controller

---
+++ SetViewConttrollerViewController.m
@@ -0,0 +1,38 @@
+//
+//  SetViewConttrollerViewController.m
+//  Matchismo
+//
+//  Created by Lexor on 12.05.13.
+//  Copyright (c) 2013 Lexor. All rights reserved.
+//
+
+#import "SetViewConttrollerViewController.h"
+
+@interface SetViewConttrollerViewController ()
+
+@end
+
+@implementation SetViewConttrollerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
